f792492826174802aaa0acb15a3a05000d9593ec
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-04-22 11:02:55 +0530

    added buy from and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 function openBuy(){
     window.open("buy.htm", "_top");
 }
+
+function Borderon(id){
+    document.getElementById(id).style.borderColor = "#37df37";
+}
+
+function Borderoff(id){
+    document.getElementById(id).style.borderColor = "transparent";
+}
