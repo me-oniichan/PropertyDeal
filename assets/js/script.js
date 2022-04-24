87eba98f7062394ee12652957b64e9f3bd69a7c9
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-04-24 21:17:31 +0530

    updated sell

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,8 @@ function Borderon(id){
 function Borderoff(id){
     document.getElementById(id).style.borderColor = "transparent";
 }
+
+document.getElementById("sell-bt").onchange =() =>{
+    let img = document.getElementById("sell-img");
+    img.src = URL.createObjectURL(document.getElementById("sell-bt").files[0]);
+}
