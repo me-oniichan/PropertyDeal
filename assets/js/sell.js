1d494464e4e8f1e9e51ec635475d72c7c09f8c3a
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-04-29 20:15:23 +0530

    Now del amenties dtata

--- assets/js/sell.js
+++ assets/js/sell.js
@@ -18,7 +18,7 @@ let addlist = document.getElementById("amen-add").onclick = () => {
     let val = document.getElementById("amenities").value;
     if(!val) return;
     let cont = document.getElementById("values");
-    let c_id = cont.getElementsByTagName("button");
+    let c_id = cont.getElementsByTagName("span");
     if (c_id[0]) {
         c_id = c_id[c_id.length - 1].id;
         c_id = c_id ? parseInt(c_id) : 1;
@@ -26,9 +26,9 @@ let addlist = document.getElementById("amen-add").onclick = () => {
     else {
         c_id = 0
     }
-    let html = cont.innerHTML + `<span class="amen-inp">
+    let html = cont.innerHTML + `<span class="amen-inp" id="${c_id + 1}">
     <input class="amen-data amen-vals" type="text" value="${val}" style = "width:${val.length*9}px" disabled>
-    <button id="${c_id + 1}" class="amen-bt" type="button">x</button>
+    <button  class="amen-bt" type="button" onclick = "delData(${c_id + 1})">x</button>
 </span>`
     cont.innerHTML = html;
     document.getElementById("amenities").value = "";
@@ -36,4 +36,9 @@ let addlist = document.getElementById("amen-add").onclick = () => {
 
 document.getElementById("amenities").onkeydown = (event) =>{
     return event.key === "Enter"? addlist():0;
+}
+
+function delData(id){
+    let elem = document.getElementById(id);
+    elem.remove();
 }
